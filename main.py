d4f70fb44cb694b8db3291cdab05f3eab8eb042c
mniyk <my.name.is.yohei.kono@gmail.com>
2021-03-15 18:49:35 +0900

    Fix: 入力する度に翻訳処理を行うように修正
    レイアウトも変更

--- main.py
+++ main.py
@@ -9,8 +9,8 @@ class MainWindow(QtWidgets.QMainWindow):
         super(MainWindow, self).__init__()
         self.setWindowTitle('Google Translate Tool')
         self.setStyleSheet('background-color: #121212;')
-        self.setMinimumSize(520, 255)
-        self.setMaximumSize(520, 255)
+        self.setMinimumSize(520, 210)
+        self.setMaximumSize(520, 210)
 
         self.translation_widget = TranslationWidget()
         self.setCentralWidget(self.translation_widget)
@@ -26,15 +26,37 @@ class TranslationWidget(QtWidgets.QWidget):
         # 英語→日本語
         self.en_jp_radio = QtWidgets.QRadioButton('英語 → 日本語', self)
         self.en_jp_radio.setChecked(True)
-        self.en_jp_radio.move(30, 20)
+        self.en_jp_radio.move(30, 17)
         self.en_jp_radio.setStyleSheet('color: white;')
         self.main_layout.addWidget(self.en_jp_radio)
 
         # 日本語→英語
         self.jp_en_radio = QtWidgets.QRadioButton('日本語 → 英語', self)
-        self.jp_en_radio.move(150, 20)
+        self.jp_en_radio.move(150, 17)
         self.jp_en_radio.setStyleSheet('color: white;')
         self.main_layout.addWidget(self.jp_en_radio)
+
+        # ペーストボタン
+        self.paste_btn = QtWidgets.QPushButton('ペースト', self)
+        self.paste_btn.resize(80, 20)
+        self.paste_btn.move(320, 15)
+        self.paste_btn.setStyleSheet(
+            'background-color: white;'
+            'border-radius: 5px 5px 5px 5px;'
+        )
+        self.paste_btn.clicked.connect(self.paste_text)
+        self.main_layout.addWidget(self.paste_btn)
+
+        # クリアボタン
+        self.clear_btn = QtWidgets.QPushButton('クリア', self)
+        self.clear_btn.resize(80, 20)
+        self.clear_btn.move(410, 15)
+        self.clear_btn.setStyleSheet(
+            'background-color: white;'
+            'border-radius: 5px 5px 5px 5px;'
+        )
+        self.clear_btn.clicked.connect(self.clear_text)
+        self.main_layout.addWidget(self.clear_btn)
 
         # 翻訳前テキスト
         self.before_translation = QtWidgets.QTextEdit(self)
@@ -46,23 +68,13 @@ class TranslationWidget(QtWidgets.QWidget):
         )
         self.before_translation.setText('')
         self.before_translation.setPlaceholderText('翻訳前のテキスト')
+        self.before_translation.textChanged.connect(self.google_translation)
         self.main_layout.addWidget(self.before_translation)
-
-        # ペーストボタン
-        self.paste_btn = QtWidgets.QPushButton('ペースト', self)
-        self.paste_btn.resize(100, 30)
-        self.paste_btn.move(30, 120)
-        self.paste_btn.setStyleSheet(
-            'background-color: white;'
-            'border-radius: 5px 5px 5px 5px;'
-        )
-        self.paste_btn.clicked.connect(self.paste_text)
-        self.main_layout.addWidget(self.paste_btn)
 
         # コピーボタン
         self.copy_btn = QtWidgets.QPushButton('コピー', self)
-        self.copy_btn.resize(100, 30)
-        self.copy_btn.move(150, 120)
+        self.copy_btn.resize(80, 20)
+        self.copy_btn.move(410, 105)
         self.copy_btn.setStyleSheet(
             'background-color: white;'
             'border-radius: 5px 5px 5px 5px;'
@@ -70,32 +82,10 @@ class TranslationWidget(QtWidgets.QWidget):
         self.copy_btn.clicked.connect(self.copy_text)
         self.main_layout.addWidget(self.copy_btn)
 
-        # クリアボタン
-        self.clear_btn = QtWidgets.QPushButton('クリア', self)
-        self.clear_btn.resize(100, 30)
-        self.clear_btn.move(270, 120)
-        self.clear_btn.setStyleSheet(
-            'background-color: white;'
-            'border-radius: 5px 5px 5px 5px;'
-        )
-        self.clear_btn.clicked.connect(self.clear_text)
-        self.main_layout.addWidget(self.clear_btn)
-
-        # 翻訳ボタン
-        self.translation_btn = QtWidgets.QPushButton('翻訳', self)
-        self.translation_btn.resize(100, 30)
-        self.translation_btn.move(390, 120)
-        self.translation_btn.setStyleSheet(
-            'background-color: white;'
-            'border-radius: 5px 5px 5px 5px;'
-        )
-        self.translation_btn.clicked.connect(self.google_translation)
-        self.main_layout.addWidget(self.translation_btn)
-
         # 翻訳後テキスト
         self.after_translation_text = QtWidgets.QTextEdit(self)
         self.after_translation_text.resize(470, 60)
-        self.after_translation_text.move(25, 170)
+        self.after_translation_text.move(25, 130)
         self.after_translation_text.setStyleSheet(
             'background-color: #121212;'
             'color: white;'
@@ -112,7 +102,6 @@ class TranslationWidget(QtWidgets.QWidget):
         pyperclip.copy(self.after_translation_text.toPlainText())
 
     def clear_text(self):
-        self.en_jp_radio.setChecked(True)
         self.before_translation.setText('')
         self.after_translation_text.setText('')
 
